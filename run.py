9c3d1fda788c66d09daf9691a92784c4e91e8428
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2022-11-02 18:38:07 +0000

    inital commit

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from tkinter import *
+import time
+
+window = Tk()
+window.title("Digital clock")
+window.geometry('400x150')
+
+
+def mytime():
+    hour = time.strftime('%I')
+    minute = time.strftime('%M')
+    second = time.strftime('%S')
+    am_pm=time.strftime('%p')
+    day=time.strftime('%A')
+    zone=time.strftime('%Z')
+    myText = hour + ":" + minute + ":" + second + ":" +am_pm
+    myText2=day + ","+zone
+    myLabel.config(text=myText)
+    myLabel2.config(text=myText2)
+    myLabel.after(1000, mytime)
+
+
+myLabel = Label(window, text="", font=("DS-digital", 50,"bold"), background='black',foreground='cyan')
+
+myLabel.pack()
+myLabel2 = Label(window,text="",font=("arail",15))
+myLabel2.pack()
+mytime()
+
+window.mainloop()
